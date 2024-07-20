6d8d25db0681a89fc51cca99ff9f76cce99b5bc1
jakwesoneil <jakwesoneilgonzalez@gmail.com>
2024-07-20 17:46:36 +0800

    fixed routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import './App.css'
 import Home from './Home'
 import EmployeesView from './component/employee/EmployeesView'
 import NavBar from './component/common/NavBar'
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className = "bg-blue-900 min-h-screen">
-        <NavBar/>
-        <h2>Welcome to GVendor!</h2>
-        <Home/> 
-        <EmployeesView/>
+        
+        <Router>
+          <NavBar/>
+          <Routes>
+            <Route exact path="/" element={<Home />}> </Route>
+            <Route exact path="/view-employees" element={<EmployeesView />}> </Route>
+          </Routes>
+        </Router>
       </div>
     </>
   )
--- frontend/src/component/common/NavBar.jsx
+++ frontend/src/component/common/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'; 
 
 const NavBar = () => {
   return (
     <div>
         <nav className="bg-white">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                <a href="https://globe.com.ph" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link  className="flex items-center space-x-3 rtl:space-x-reverse" to="https://globe.com.ph">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Globe_Telecom_logo.svg/1200px-Globe_Telecom_logo.svg.png" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl  font-semibold whitespace-nowrap text-blue-900">GVendor</span>
-                </a>
+                </Link>
                 <div className="flex items-center space-x-6 rtl:space-x-reverse">
                     <a href="tel:5541251234" className="text-sm  text-blue-900 hover:underline">(555) 412-1234</a>
                     <a href="#" className="text-sm  text-blue-900  hover:underline">Login</a>
@@ -20,13 +21,13 @@ const NavBar = () => {
                 <div className="flex items-center">
                     <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                         <li>
-                            <a href="#" className="text-white  hover:underline" aria-current="page">Dashboard</a>
+                            <Link className="text-white  hover:underline"  aria-current="page" to={"/"} >Home</Link>
                         </li>
                         <li>
-                            <a href="#" className="text-white hover:underline">View All Employees</a>
+                            <Link className="text-white  hover:underline"  aria-current="page" to={"/view-employees"} >View All Employees</Link>
                         </li>
                         <li>
-                            <a href="#" className="text-white  hover:underline">Add New Employees</a>
+                            <Link className="text-white  hover:underline" to={"/add-employees"} >Add an Employee</Link>
                         </li>
                     </ul>
                 </div>
